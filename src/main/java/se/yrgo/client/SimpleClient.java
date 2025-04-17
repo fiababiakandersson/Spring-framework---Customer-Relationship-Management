041508f08c23676e40b22118a11fe51a5393ecb3
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2025-04-17 12:54:18 +0200

    uppgift 3 (write out customers)

--- src/main/java/se/yrgo/client/SimpleClient.java
+++ src/main/java/se/yrgo/client/SimpleClient.java
@@ -1,10 +1,27 @@
 package se.yrgo.client;
 
+import java.util.*;
 
+import org.springframework.context.support.*;
+
+import se.yrgo.domain.*;
+import se.yrgo.services.customers.*;
 
 public class SimpleClient {
 
     public static void main(String[] args) {
+        ClassPathXmlApplicationContext container = new ClassPathXmlApplicationContext("application.xml");
 
+        CustomerManagementService customerService = container.getBean(CustomerManagementService.class);
+
+        customerService.newCustomer(new Customer("BD37", "Name1", "Note1"));
+
+        List<Customer> allCustomers = customerService.getAllCustomers();
+
+        for (Customer customer : allCustomers) {
+            System.out.println(customer);
+        }
+
+        container.close();
     }
 }
