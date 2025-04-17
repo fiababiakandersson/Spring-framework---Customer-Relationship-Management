6a74fd9cc39bc85ee01609493ca2adfa2ddbe454
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2025-04-17 10:52:06 +0200

    uppgift 1 (implement all methods from CustomerManagementService)

--- src/main/java/se/yrgo/services/customers/CustomerManagementMockImpl.java
+++ src/main/java/se/yrgo/services/customers/CustomerManagementMockImpl.java
@@ -1,69 +1,77 @@
 package se.yrgo.services.customers;
 
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
-import se.yrgo.domain.Call;
-import se.yrgo.domain.Customer;
+import se.yrgo.domain.*;
 
 public class CustomerManagementMockImpl implements CustomerManagementService {
-	private HashMap<String,Customer> customerMap;
+	private HashMap<String, Customer> customerMap;
 
 	public CustomerManagementMockImpl() {
-		customerMap = new HashMap<String,Customer>();
-		customerMap.put("OB74", new Customer("OB74" ,"Fargo Ltd", "some notes"));
-		customerMap.put("NV10", new Customer("NV10" ,"North Ltd", "some other notes"));
-		customerMap.put("RM210", new Customer("RM210" ,"River Ltd", "some more notes"));
+		customerMap = new HashMap<String, Customer>();
+		customerMap.put("OB74", new Customer("OB74", "Fargo Ltd", "some notes"));
+		customerMap.put("NV10", new Customer("NV10", "North Ltd", "some other notes"));
+		customerMap.put("RM210", new Customer("RM210", "River Ltd", "some more notes"));
 	}
-
 
 	@Override
 	public void newCustomer(Customer newCustomer) {
-
+		customerMap.put(newCustomer.getCustomerId(), newCustomer);
 	}
 
 	@Override
 	public void updateCustomer(Customer changedCustomer) {
-
-
+		Customer c = customerMap.get(changedCustomer.getCustomerId());
+		c.setCustomerId(changedCustomer.getCustomerId());
+		c.setCompanyName(changedCustomer.getCompanyName());
+		c.setNotes(changedCustomer.getNotes());
+		c.setEmail(changedCustomer.getEmail());
+		c.setTelephone(changedCustomer.getTelephone());
+		c.setCalls(changedCustomer.getCalls());
 	}
 
 	@Override
 	public void deleteCustomer(Customer oldCustomer) {
-		// TODO Auto-generated method stub
+		customerMap.remove(oldCustomer.getCustomerId());
 
 	}
 
 	@Override
 	public Customer findCustomerById(String customerId) throws CustomerNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		return customerMap.get(customerId);
 	}
 
 	@Override
 	public List<Customer> findCustomersByName(String name) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Customer> customers = new ArrayList<>();
+
+		for (Map.Entry<String, Customer> entry : customerMap.entrySet()) {
+			if (entry.getValue().getCompanyName() == name)
+				customers.add(entry.getValue());
+		}
+		return customers;
 	}
 
 	@Override
 	public List<Customer> getAllCustomers() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Customer> customers = new ArrayList<>();
+
+		for (Map.Entry<String, Customer> entry : customerMap.entrySet()) {
+			customers.add(entry.getValue());
+		}
+		return customers;
 	}
 
 	@Override
 	public Customer getFullCustomerDetail(String customerId) throws CustomerNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		return customerMap.get(customerId);
 	}
 
 	@Override
 	public void recordCall(String customerId, Call callDetails) throws CustomerNotFoundException {
-		//First find the customer
+		Customer c = customerMap.get(customerId);
 
-		//Call the addCall on the customer
-
+		c.addCall(callDetails);
 	}
 
 }
